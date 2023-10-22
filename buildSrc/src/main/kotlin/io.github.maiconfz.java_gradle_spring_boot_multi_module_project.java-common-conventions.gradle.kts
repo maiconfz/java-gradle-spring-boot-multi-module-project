ee0f30c1fe7040404c75373043b8675a13300e99
Maiconfz <maiconfz@gmail.com>
2023-10-23 00:02:42 +0100

    Add commons libs to all projects

--- buildSrc/src/main/kotlin/io.github.maiconfz.java_gradle_spring_boot_multi_module_project.java-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/io.github.maiconfz.java_gradle_spring_boot_multi_module_project.java-common-conventions.gradle.kts
@@ -11,6 +11,11 @@ repositories {
 
 dependencies {
     implementation("com.google.guava:guava:32.1.3-jre")
+    implementation("org.apache.commons:commons-lang3:3.13.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("org.apache.commons:commons-text:1.10.0")
+    implementation("org.tinylog:tinylog-api:2.6.2")
+    implementation("org.tinylog:tinylog-impl:2.6.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
