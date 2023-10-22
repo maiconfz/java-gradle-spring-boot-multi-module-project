7e6915494f0a3f3be06e41be144be960b035c4f6
Maiconfz <maiconfz@gmail.com>
2023-10-23 00:03:00 +0100

    Add new method to find status by StatusEnum

--- service/src/main/java/io/github/maiconfz/java_gradle_spring_boot_multi_module_project/service/StatusService.java
+++ service/src/main/java/io/github/maiconfz/java_gradle_spring_boot_multi_module_project/service/StatusService.java
@@ -1,7 +1,10 @@
 package io.github.maiconfz.java_gradle_spring_boot_multi_module_project.service;
 
 import io.github.maiconfz.java_gradle_spring_boot_multi_module_project.data.model.Status;
+import io.github.maiconfz.java_gradle_spring_boot_multi_module_project.data.model.enums.StatusEnum;
 import io.github.maiconfz.java_gradle_spring_boot_multi_module_project.service.base.NamedEntityService;
 
-public interface StatusService extends NamedEntityService<Status> {    
+public interface StatusService extends NamedEntityService<Status> {
+
+    Status findByStatusEnum(StatusEnum statusEnum);
 }
