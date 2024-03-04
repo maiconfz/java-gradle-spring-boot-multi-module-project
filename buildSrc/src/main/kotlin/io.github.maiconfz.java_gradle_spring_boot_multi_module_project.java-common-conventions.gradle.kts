de49beb511a2c0dc1427a9e7168e4c013236d4f4
Maiconfz <maiconfz@gmail.com>
2024-03-04 02:05:18 +0000

    Update dependencies and java 17 to java 21

--- buildSrc/src/main/kotlin/io.github.maiconfz.java_gradle_spring_boot_multi_module_project.java-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/io.github.maiconfz.java_gradle_spring_boot_multi_module_project.java-common-conventions.gradle.kts
@@ -10,18 +10,18 @@ repositories {
 }
 
 dependencies {
-    implementation("com.google.guava:guava:32.1.3-jre")
-    implementation("org.apache.commons:commons-lang3:3.13.0")
+    implementation("com.google.guava:guava:33.0.0-jre")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
     implementation("org.apache.commons:commons-collections4:4.4")
-    implementation("org.apache.commons:commons-text:1.10.0")
-    implementation("org.tinylog:tinylog-api:2.6.2")
-    implementation("org.tinylog:tinylog-impl:2.6.2")
+    implementation("org.apache.commons:commons-text:1.11.0")
+    implementation("org.tinylog:tinylog-api:2.7.0")
+    implementation("org.tinylog:tinylog-impl:2.7.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
